c8923ae87cdaee0580561015b4f8f7f6c983e31e
Kingsley <sengking.tan@gmail.com>
2017-04-16 14:09:22 +0800

    completed new, edit, destroy actions for turnout model

--- app/controllers/turnouts_controller.rb
+++ app/controllers/turnouts_controller.rb
@@ -8,18 +8,45 @@ class TurnoutsController < ApplicationController
   end
 
   def new
+    @turnout = Turnout.new
   end
 
   def create
+    @turnout = Turnout.new(turnout_params)
+
+    if @turnout.save
+      redirect_to turnouts_path
+    else
+      redirect_to new_turnout_path
+    end
   end
 
   def edit
+    @turnout = Turnout.find_by(id: params[:id])
   end
 
   def update
+    @turnout = Turnout.find(params[:id])
+    if @turnout.update(turnout_params)
+      redirect_to turnouts_path
+    else
+      redirect_to edit_turnout_path(@turnout)
+    end
   end
 
   def destroy
+    @turnout = Turnout.find(params[:id])
+    if @turnout.destroy
+      redirect_to turnouts_path
+    else
+      redirect_to edit_turnout_path(@turnout)
+    end
+  end
+
+  private
+
+  def turnout_params
+    params.require(:turnout).permit(:user_id, :location)
   end
 
 end
