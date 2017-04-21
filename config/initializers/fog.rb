b0508bd1c765f5bc36a9cbdfa77f5072f817614f
Kingsley <sengking.tan@gmail.com>
2017-04-21 16:58:27 +0800

    amended fog.rb, moving config.storage = :fog downwards

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -7,13 +7,14 @@ if Rails.env.test? || Rails.env.development?
 else
 
   CarrierWave.configure do |config|
-    config.storage = :fog
+
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV.fetch("AWS_ACCESS_KEY_ID"),
       aws_secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY"),
       region: 'ap-southeast-1',
     }
+    config.storage = :fog
     config.fog_directory = ENV.fetch("AWS_BUCKET")
     config.fog_public = false
   end
